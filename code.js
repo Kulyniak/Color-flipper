c6ca74abd0d4d327005106e09d6f45ccc8ac3d93
Kulyniak <80394011+Kulyniak@users.noreply.github.com>
2022-08-15 17:01:05 +0300

    new changes

---
+++ code.js
@@ -0,0 +1,13 @@
+const colors = ['green' , 'red', 'white', 'purple', 'brown', 'grey', 'yellow']
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomcolor();
+    console.log(getRandomcolor);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+})
+function getRandomcolor(){
+    return Math.floor(Math.random() * colors.length);
+}
